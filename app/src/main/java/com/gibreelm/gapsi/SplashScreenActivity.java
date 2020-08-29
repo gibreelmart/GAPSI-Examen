e08870f5231b6ed347c8f6d2dc5cca0b01f561a9
gibreelmart <ABCdef123>
2020-08-29 10:32:56 -0500

    Splash Themes/Styles

---
+++ app/src/main/java/com/gibreelm/gapsi/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.gibreelm.gapsi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed (() -> {
+            Intent i = new Intent (SplashScreenActivity.this, MainActivity.class);
+            startActivity(i);
+            finish ();
+        }, 2000);
+    }
+
+    @Override
+    public void onBackPressed() {
+        try {
+        } catch (Exception ex) {
+        }
+    }
+
+}
